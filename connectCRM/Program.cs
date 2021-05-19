0acbbf79f69d566e1f465003c11927367550ce84
Łukasz Płoński <lplonski@x-code.pl>
2021-05-19 17:48:43 +0200

    Add project files.

---
+++ connectCRM/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Xrm.Sdk.Client;
+using System.ServiceModel.Description;
+using Microsoft.Crm.Sdk.Messages;
+
+namespace Carl.Crm.OrgServiceProxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Uri oUri = new Uri("https://crm365-iis.x-code.pl/box/XRMServices/2011/Organization.svc");
+                ClientCredentials clientCredentials = new ClientCredentials();
+                clientCredentials.UserName.UserName = "lplonski";
+                clientCredentials.UserName.Password = "Bicek#2021";
+
+                OrganizationServiceProxy _serviceProxy = new OrganizationServiceProxy(oUri, null, clientCredentials, null);
+                _serviceProxy.EnableProxyTypes();
+
+                OrganizationServiceContext orgContext = new OrganizationServiceContext(_serviceProxy);
+
+                RetrieveVersionRequest versionRequest = new RetrieveVersionRequest();
+                RetrieveVersionResponse versionResponse = (RetrieveVersionResponse)_serviceProxy.Execute(versionRequest);
+
+                Console.WriteLine("Microsoft Dynamics CRM version {0}.", versionResponse.Version);
+                Console.ReadLine();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
